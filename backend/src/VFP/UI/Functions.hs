2cdd493592d11d5f541cbca6686c20cbd7d596ad
Lukas Streckeisen <lukas@streckeisen.dev>
2023-11-21 16:07:32 +0100

    adjust UI to new model & add support for lambdas

--- backend/src/VFP/UI/Functions.hs
+++ backend/src/VFP/UI/Functions.hs
@@ -4,32 +4,15 @@ import VFP.UI.UIModel
 
 functions :: [( String, ValueDefinition )]
 functions = [
-        ("id1", ValueDefinition { definitionReference = ValueReference { referenceId = 1, referenceName = "Function 1" }, definitionValue = TypeHoleValue 123 } )
-        --("id2", ValueDefinition { functionId = "id2", functionName = "Function 2", definition = UserFunction (TypeHoleArg (TypeHole "typeHole2" "Int"))} ),
-        --("id3", ValueDefinition { functionId = "id3", functionName = "Function 3", definition = UserFunction (FunctionValue (FunctionCallValue (FunctionCall { functionCallId = "id2", functionArgs = [TypeHoleParam (TypeHole "typeHole1" "String"), TypeHoleParam (TypeHole "typeHole2" "String")] }))) }),
-        --("id4", ValueDefinition {
-        --  functionId = "id3",
-        --  functionName = "Function 3",
-        --  definition = UserFunction (
-        --      FunctionValue (
-        --          FunctionCallValue (
-        --              FunctionCall {
-        --                  functionCallId = "id2",
-        --                  functionArgs = [
-        --                      ArgumentValue (
-        --                          FunctionRefValue (
-        --                              FunctionRef {
-        --                                  functionRefId = "id1"
-        --                              }
-        --                          )
-        --                      ),
-        --                      TypeHoleParam (TypeHole "typeHole2" "String")
-        --                  ]
-        --              }
-        --          )
-        --      )
-        --  )
-        --})
+        ("id1", ValueDefinition { definitionType = "String", definitionName = "functionOne", definitionValue = TypeHole "123" } ),
+        ("id2", ValueDefinition { definitionType = "String -> Int -> String", definitionName = "functionTwo", definitionValue = TypeHole "124" } ),
+        ("id3", ValueDefinition { definitionType = "String", definitionName = "functionThree", definitionValue = Reference "funcitonTwo" (ArgumentList [TypeHole "125", TypeHole "126"]) } ),
+        ("id4", ValueDefinition { definitionType = "String -> Int -> String", definitionName = "functionFour", definitionValue = Lambda "s" (Reference "functionTwo" (ArgumentList [Reference "s" (ArgumentList []), TypeHole "127"])) } )
+    ]
+
+typedValues :: [( String, TypedValue)]
+typedValues = [
+    ("id4", TypedLambda "String -> Int -> String" ("String", "s") (TypedReference "String -> Int -> String" "functionTwo" [TypedReference "String" "s" [], TypedTypeHole "Int" "127"]))
     ]
 
 lookupFunction :: String -> Maybe ValueDefinition
